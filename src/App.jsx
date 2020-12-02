75285b32e9e29d4e7ad41fc437702bee31ac20c5
Byron McDaniel <byronkmcdaniel@gmail.com>
2020-12-01 20:53:57 -0500

    added state to user item

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import UserItem from './components/users/UserItem'
 
 class App extends Component {
   render() {
     const name = 'John Doe'
     return (
       <div>
-        <Navbar />
+        <Navbar title="GitHub Hunter" icon="fab fa-github" />
+        <UserItem />
       </div>
     )
   }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,13 @@ import { Component } from 'react'
 
 class Navbar extends Component {
   render() {
-    return <nav className="navbar bg-primary">hello</nav>
+    return (
+      <nav className="navbar bg-primary">
+        <h1>
+          <i className={this.props.icon} /> {this.props.title}
+        </h1>
+      </nav>
+    )
   }
 }
 
