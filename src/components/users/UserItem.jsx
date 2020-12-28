3089131f6e9839098140a87fa94be939478dbcc7
Byron McDaniel <byronkmcdaniel@gmail.com>
2020-12-28 02:16:28 -0500

    ADDED CLIENT SECRET AND ID KEY

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,31 +1,23 @@
-const { Component } = require('react')
+import React from 'react'
 
-class UserItem extends Component {
-  state = {
-    id: 'id',
-    login: 'mojombo',
-    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-    html_url: 'https://github.com/mojombo',
-  }
-
-  render() {
-    return (
-      <div className="card text-center">
-        <img
-          src={this.state.avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: '60px' }}
-        />
-        <h3>{this.state.login}</h3>
-        <div>
-          <a href={this.state.html_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-        </div>
+function UserItem(props) {
+  const { login, avatar_url, html_url } = props.user
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          More
+        </a>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default UserItem
