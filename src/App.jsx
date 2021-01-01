097763379568f727b7a294dd98ccf1f0b48b44d7
Byron McDaniel <byronkmcdaniel@gmail.com>
2021-01-01 00:49:22 -0500

    account lookup

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,18 @@ class App extends Component {
       &client_secrt=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
     this.setState({ users: res.data, loading: false })
   }
+  searchUsers = async (text) => {
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    this.setState({ users: res.data.items, loading: false })
+  }
 
   render() {
     return (
       <div>
         <Navbar title="GitHub Hunter" icon="fab fa-github" />
         <div className="container">
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -1,16 +1,31 @@
 import React, { Component } from 'react'
 
 class Search extends Component {
+  state = {
+    text: '',
+  }
+  onChange = (e) => {
+    this.setState({ text: e.target.value })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    this.props.searchUsers(this.state.text)
+    this.setState({ text: '' })
+  }
+
   render() {
     return (
       <div>
-        <form className="form">
-          <input type="text" name="text" placeholder="Search User..." />
+        <form onSubmit={this.onSubmit} className="form">
           <input
-            type="submit"
-            value="search"
-            className="btn btn dark btn black"
+            type="text"
+            name="text"
+            placeholder="Search User..."
+            value={this.state.text}
+            onChange={this.onChange}
           />
+          <input type="submit" value="search" className="btn" />
         </form>
       </div>
     )
